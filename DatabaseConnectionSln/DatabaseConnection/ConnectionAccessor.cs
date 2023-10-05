795008ce41b154e3797a2ae833ada4fadc116341
Lee Zi Yang <leezy2001@gmail.com>
2023-10-05 15:10:05 -0500

    Add Database Connection

---
+++ DatabaseConnectionSln/DatabaseConnection/ConnectionAccessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class ConnectionAccessor
+{
+    public static bool TestDatabaseConnection()
+    {
+        // Connection string - replace with your actual database connection details
+        string connectionString =
+            "Data Source=LeeZiYang\\SQLEXPRESS;Initial Catalog=Employees;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Database connection successful.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ DatabaseConnectionSln/DatabaseConnection/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DatabaseConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (ConnectionAccessor.TestDatabaseConnection())
+            {
+                // Database connection is successful. You can perform your database operations here.
+            }
+            else
+            {
+                // Database connection failed.
+                Console.WriteLine("Unable to connect to the database.");
+            }
+        }
+    }
+}
